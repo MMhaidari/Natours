7aef1c06e4bd026dd9a6a90a24025ef7ae186bc5
Mohammad Mohsen Haidari <mohsenhaidari0766@gamil.com>
2024-04-08 13:00:01 +0430

    review controller added also security issues addressed like xss and hpp

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
-const authrController = require('./../controllers/authController');
-
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', authrController.signup)
-router.post('/login', authrController.login)
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.post('/forgotPassword', authrController.forgotPassword)
-router.patch('/resetPassword/:token', authrController.resetPassword)
+// Protect all routes after this middleware
+router.use(authController.protect);
 
-router.patch('/updatePassword', authrController.protect, authrController.updatePassword)
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-router.patch('/updateMe', authrController.protect, userController.updateMe)
-
-router.delete('/deleteMe', authrController.protect, userController.deleteMe)
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = "mongodb+srv://mohsenhaidari0766:admin123@cluster0.n1zbq4y.mongodb.net/natoursPractice?retryWrites=true&w=majority";
+const DB = process.env.DATABASE;
 
 mongoose
   .connect(DB)
-  .then(() => console.log('DB connection successful!'))
-  .catch((error) => {
-    console.log(error);
-  });
-
+  .then(() => console.log('DB connection successful!')).catch((error) => console.log(error));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
